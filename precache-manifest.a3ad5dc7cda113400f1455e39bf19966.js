b2e16e67c69571708715eb0e62f1fc1b9bb8ffc0
Yors <jatbribiesca@gmail.com>
2021-01-07 15:33:04 -0600

    Updates

---
+++ precache-manifest.a3ad5dc7cda113400f1455e39bf19966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c85a0506d13089ce0ffe1e56507170f",
+    "url": "/formula1/public/index.html/index.html"
+  },
+  {
+    "revision": "9305975e4cc01afc1c3a",
+    "url": "/formula1/public/index.html/static/css/main.ce371bda.chunk.css"
+  },
+  {
+    "revision": "8b3474d591937dd8ac17",
+    "url": "/formula1/public/index.html/static/js/2.3b000a22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/formula1/public/index.html/static/js/2.3b000a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9305975e4cc01afc1c3a",
+    "url": "/formula1/public/index.html/static/js/main.894d9072.chunk.js"
+  },
+  {
+    "revision": "379be97ed82b4d66cc44",
+    "url": "/formula1/public/index.html/static/js/runtime-main.08cc4581.js"
+  }
+]);
